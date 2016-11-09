e7d289bd2bfc0640482d209863334f6e9cf2728d
jewel7meme <zyang3@css.edu>
2016-11-08 18:42:24 -0600

    Request  407:  read the trades from a remote call to a web service instead of using a Stream for input.

---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace SingleResponsibilityPrinciple
+{
+    class URLTradeDataProvider: ITradeDataProvider
+    {
+        private string url;
+
+        public URLTradeDataProvider(string url)
+        {
+            this.url = url;
+        }
+        public IEnumerable<string> GetTradeData()
+        {
+            var tradeData = new List<string>();
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+        }
+    }
+}
